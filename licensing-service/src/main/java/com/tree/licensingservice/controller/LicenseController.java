e905306aac34f16ffd6b70c550662c57fb6079fc
tree0122 <cuiyaohua11@163.com>
2019-01-02 18:03:40 +0800

    config-server

--- licensing-service/src/main/java/com/tree/licensingservice/controller/LicenseController.java
+++ licensing-service/src/main/java/com/tree/licensingservice/controller/LicenseController.java
@@ -1,20 +1,26 @@
 package com.tree.licensingservice.controller;
 
 import com.tree.licensingservice.model.License;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/v1/organizations/{organizationId}/licenses")
 public class LicenseController {
 
+    @Value("${trace.property: 1111}")
+    private String traceProperty;
+
     @GetMapping("/{id}")
     public License get(
             @PathVariable("organizationId") String organizationId,
-            @PathVariable("id") String id
-    ){
+            @PathVariable("id") String id){
+        log.info("traceProperty: {}", traceProperty);
         return License.builder()
                 .id(id)
                 .organizationId(organizationId)
