231f41e67023e1fb148fefe399c4a1db624f9a98
tree0122 <cuiyaohua11@163.com>
2019-07-23 01:22:15 +0800

    eureka server and service

---
+++ eureka/hello-service/src/test/java/com/tree/cloud/helloservice/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tree.cloud.helloservice;
+
+import org.junit.Test;
+/*
+@RunWith(SpringRunner.class)
+@SpringBootTest*/
+public class HelloServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ eureka/server/src/test/java/com/tree/cloud/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tree.cloud.server;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class ServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
