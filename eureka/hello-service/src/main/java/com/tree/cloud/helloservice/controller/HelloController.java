5f9fffc72319dcd80444f0da84bd203e69c45af3
tree0122 <cuiyaohua11@163.com>
2019-07-23 01:20:44 +0800

    eureka server and service

---
+++ eureka/hello-service/src/main/java/com/tree/cloud/helloservice/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.tree.cloud.helloservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String index(){
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        log.info("serviceInstance: {}", instance);
+        return "hello world";
+    }
+}
