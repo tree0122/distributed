089dad2ba17409761bfbdbc38e76f33875c21579
tree0122 <cuiyaohua11@163.com>
2019-08-04 21:22:52 +0800

    hystrix service

---
+++ eureka/hystrix-service/src/main/java/com/tree/cloud/hystrixservice/HystrixServiceApplication.java
@@ -0,0 +1,26 @@
+package com.tree.cloud.hystrixservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HystrixServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixServiceApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
